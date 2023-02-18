c15a047987603bc8dc3490150aa8bcdbc12e6927
Kaiky Lagares Guimarães <114961527+KaikyLagares@users.noreply.github.com>
2023-02-18 20:50:40 -0300

    Add files via upload

---
+++ _ExamDAta_.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `cadastrobancario`
+ */
+
+/* `cadastrobancario`.`clientes` */
+$clientes = array(
+  array('codigo' => NULL,'nome' => 'Kaiky','cpf' => '101','idade' => '0000-00-00','sexo' => 'M','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'Kaiky','cpf' => '101','idade' => '0000-00-00','sexo' => 'M','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'Kaiky','cpf' => '101','idade' => '0000-00-00','sexo' => 'M','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'Maria','cpf' => '101','idade' => '0000-00-00','sexo' => 'M','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'ANA','cpf' => '103','idade' => '0000-00-00','sexo' => 'F','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'ISA','cpf' => '103','idade' => '0000-00-00','sexo' => 'F','nacionalidade' => 'Brasil','profissao' => NULL),
+  array('codigo' => NULL,'nome' => 'Gabriela','cpf' => '103','idade' => '2003-02-03','sexo' => 'F','nacionalidade' => 'Brasil','profissao' => NULL)
+);
+/**
+ * Database `meubanco`
+ */
+
+/* `meubanco`.`clientes` */
+$clientes = array(
+  array('Nome' => 'Maria','Sexo' => 'F','Conta' => '23456123','Telefone' => '318992231','ID' => '1'),
+  array('Nome' => 'Kaiky','Sexo' => 'M','Conta' => '445787422','Telefone' => '314589544','ID' => '2'),
+  array('Nome' => 'Gabriela','Sexo' => 'F','Conta' => '567253411','Telefone' => '215755568','ID' => '3'),
+  array('Nome' => 'Lucas','Sexo' => 'M','Conta' => '315473456','Telefone' => '4126562567','ID' => '4')
+);
